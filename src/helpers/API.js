b280d9d3159b8d9bee3ba9180f082ac52f43f546
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-27 09:56:19 +0200

    add logic to API call

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,7 +1,24 @@
 import 'regenerator-runtime/runtime';
 
+function getShowId(show) {
+  if (show === 'TOS') {
+    return 490;
+  } if (show === 'TNG') {
+    return 491;
+  } if (show === 'VOY') {
+    return 492;
+  } if (show === 'DS9') {
+    return 493;
+  } if (show === 'ENT') {
+    return 714;
+  } if (show === 'DIS') {
+    return 7480;
+  }
+  return 0;
+}
+
 const callApi = async (show, season = null) => {
-  const URL = `https://api.tvmaze.com/shows/${show}/`;
+  const URL = `https://api.tvmaze.com/shows/${getShowId(show)}/`;
   const EPISODES = 'episodes';
   const SEASONS = 'seasons';
   let Url = '';
