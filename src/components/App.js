9987f9bf47bb6e36c3727e7ea7239752397f32e2
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-27 07:27:51 +0200

    Add store to main index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../containers/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <div>
+        <Route path="/" component={Header} />
+        <Route exact path="/" component={TVShows} />
+        <Route path="/TVShows" exact component={} />
+        <Route path="/TVShows/:id" exact component={} />
+      </div>
+    </Switch>
+  </Router>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
 serviceWorker.unregister();
