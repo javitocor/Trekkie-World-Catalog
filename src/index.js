7370fa8f7e5df9840fbca86f1c992d6d25492ea5
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-30 15:54:56 +0200

    add custom font

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './configureStore';
+import './style/index.css';
 
 const store = configureStore();
 
