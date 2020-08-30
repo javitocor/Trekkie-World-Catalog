b23531ffe6117700990606cf1e8564835aebf634
javitocor <56809963+javitocor@users.noreply.github.com>
2020-08-30 13:02:40 +0200

    fix linter errors

--- src/tests/containers/EpisodeFilter.test.js
+++ src/tests/containers/EpisodeFilter.test.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import Enzyme, { shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 
 import {
   render, screen,
 } from '../customRender';
 import EpisodeFilter from '../../containers/EpisodeFilter';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('renders EpisodeFilter component', () => {
   it('Renders <EpisodeFilter /> component properly', () => {
     render(<BrowserRouter><EpisodeFilter /></BrowserRouter>);
     expect(screen.getByText(/Filter by Season/i)).toBeInTheDocument();
   });
-});
-
-it('should select correct time', () => {
-  const wrapper = shallow(<BrowserRouter><EpisodeFilter value="All" onChange={jest.fn()} /></BrowserRouter>);
-
-  expect(wrapper.find('select').props().value).toBe('All');
 });
